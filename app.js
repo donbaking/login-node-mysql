57c2ad72e1fc69af734411685e4c29526ff645d9
Willy <zxc7953119@gmail.com>
2024-04-01 16:46:36 +0800

    add message and local login and signup page

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ require("./config/passport");
 const session = require("express-session");
 const passport = require("passport");
 const profileroute = require("./routes/profile-rout");
+const flash = require("connect-flash");
+
 //設定mysql連接之資料庫
 //middleware
 app.use(express.json());
@@ -23,10 +25,17 @@ app.use(
 );
 app.use(passport.initialize()); //passport運行認證功能
 app.use(passport.session()); //讓passport使用sessione功能
-
+app.use(flash());
+app.use((req, res, next) => {
+  //使用flash功能
+  res.locals.success_msg = req.flash("success_msg"); //local是res的一個屬性
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+}); //連接message.ejs裡的三個message
 //使用router
 app.get("/", (req, res) => {
-  return res.render("index");
+  return res.render("index", { user: req.user });
 });
 
 app.use("/auth", require("./routes/auth"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 //導入router功能
 const passport = require("passport");
-
+const bcrypt = require("bcryptjs");
+const db = require("../module/cndatabase");
 //導向登入介面
 router.get("/login", (req, res) => {
-  return res.render("login");
+  return res.render("login", { user: req.user });
 });
 router.get("/logout", (req, res) => {
   req.logOut((err) => {
@@ -26,4 +27,51 @@ router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   return res.redirect("/profile"); //google登入後會去的頁面
 }); //已經經過google驗證才能使用的route
 
+//本地端登入
+router.get("/signup", (req, res) => {
+  return res.render("signup", { user: req.user });
+});
+//接收本地端的form資料
+router.post("/signup", (req, res) => {
+  let { name, email, password } = req.body;
+  if (password.length < 8) {
+    req.flash("error_msg", "密碼長度過短,至少需要八個字元");
+    return res.redirect("/auth/signup");
+  }
+  //確認信箱是否被註冊
+  db.query(
+    "SELECT * FROM users WHERE emailaddress = ?",
+    [email],
+    async (e, result) => {
+      // console.log(result);
+      if (result.length > 0) {
+        console.log("電子郵件已經被註冊過!");
+        req.flash(
+          "error_msg",
+          "信箱已經被註冊，請使用其他信箱註冊或使用此信箱登入"
+        );
+        return res.redirect("/auth/signup");
+      } else {
+        console.log("新用戶，將資料存入資料庫中");
+        try {
+          //異步hash不拖慢整體反映
+          let hashedpasswrod = await bcrypt.hash(password, 8); //做8次hash
+          db.query(
+            "INSERT INTO users SET ?",
+            { name: name, emailaddress: email, userpassword: hashedpasswrod },
+            (e, result) => {
+              if (e) throw e;
+              console.log(result);
+              req.flash("success_msg", "註冊成功!可以嘗試登入系統");
+              console.log("註冊成功");
+              return res.redirect("/auth/login");
+            }
+          );
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+  );
+});
 module.exports = router;
